5ee5a3b2b61f400c71ad020a27c8949d0bab8a9d
theaugsoc <the.augsoc@gmail.com>
2026-08-31 16:45:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import sqlite3
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_db_connection():
+    return psycopg2.connect(DATABASE_URL, sslmode='require')
 import logging
 import asyncio
 import os
